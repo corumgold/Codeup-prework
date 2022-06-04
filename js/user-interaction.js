ca9e0cfb53bfc4eacd33d0e7b0fcfa425d7b76ec
Corum Gold <goldcorum@gmail.com>
2022-06-04 17:15:16 -0400

    Finish username prompt portion

--- js/user-interaction.js
+++ js/user-interaction.js
@@ -3,6 +3,11 @@
 // TODO: Ask the user for their name.
 //       Keep asking if an empty input is provided.
 
+let userName; 
+while (userName === undefined || userName === null || userName === ''){
+   userName = prompt('What is your name?')
+}
+
 // TODO: Show an alert message that welcomes the user based on their input.
 
 // TODO: Ask the user if they like pizza.
