63a1cd66fd42ef789fd86d46cfd47e5783068dcd
Corum Gold <goldcorum@gmail.com>
2022-06-05 13:07:44 -0400

    Change functions to be IIFEs

--- js/scope.js
+++ js/scope.js
@@ -1,28 +1,17 @@
 "use strict";
 
-var myNameIs = 'Cory'; // TODO: Fill in your name here.
+(function () {
+    let myNameIs = 'Cory'
+    function sayHello(name) {
+        console.log(`Hello, ${name}!`)
+    }
+    sayHello(myNameIs);
+})();
 
-// TODO:
-// Create a function called 'sayHello' that takes a parameter 'name'.
-// When called, the function should log a message that says hello from the passed in name.
-function sayHello(name) {
-    console.log(`Hello, ${name}!`)
-}
-
-// TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
-sayHello(myNameIs);
-// Don't modify the following line
-// It generates a random number between 1 and 100 and stores it in random
-var random = Math.floor((Math.random() * 100) + 1);
-
-// TODO:
-// Create a function called 'isOdd' that takes a number as a parameter.
-// The function should use the ternary operator to log a message.
-// The log should tell the number passed in and whether it is odd or not.
-function isOdd(num) {
-    num % 2 !== 0 ? console.log("It's Odd!") : console.log("It's Not Odd :-(")
-}
-
-// TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
-
-isOdd(random);
+(() => {
+    let random = Math.floor((Math.random() * 100) + 1);
+    function isOdd(num) {
+        num % 2 !== 0 ? console.log("It's Odd!") : console.log("It's Not Odd :-(")
+    }
+    isOdd(random);
+})();
